203d44a7ac7c2468f0e3956cc8f2d16b7986697b
Octavio Mastrocesare <okimastro@gmail.com>
2023-04-16 01:03:46 -0300

    practica 1 ejericios 1 al 3

---
+++ Tareas/practica1/ejercicio1.pas
@@ -0,0 +1,21 @@
+
+Program ejercicio1;
+
+
+Var num1, num2: integer;
+
+Begin
+  write('inserte numero 1: ');
+  readln(num1);
+  write('inserte numero 2: ');
+  readln(num2);
+  If num1>num2 Then
+    writeln('el numero mas grande es: ', num1)
+  Else If num2>num1 Then
+         writeln('el numero mas grande es: ', num2)
+  Else
+    writeln('son iguales pelotudo');
+End.
+
+
+//tengo sueño pascal, dejame en paz
---
+++ Tareas/practica1/ejercicio2.pas
@@ -0,0 +1,12 @@
+
+Program ejercicio2;
+
+
+Var num: real;
+
+Begin
+  write ('inserte el numero en cuestion mi estimado: ');
+  readln (num);
+  If num>=0 Then writeln (num, '  sublime')
+  Else writeln (-num, '  mogolico');
+End.
